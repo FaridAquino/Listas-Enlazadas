b37c50fd6446cfa9dde8a88af100f4904fce91c7
R10-Mat <rayhan.matos@utec.edu.pe>
2025-08-28 18:25:28 -0500

    Dos funciones implementadas

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 
+using  namespace std;
+
+template <typename G>
 struct Nodo {
-    int dato;
+    G dato;
     Nodo* next;
+    explicit Nodo(G _dato) : dato(_dato), next(nullptr) {
+    }
 };
 template <typename T>
 class Lista {
 private:
-    Nodo* head;
+    Nodo<T>* head;
 public:
-    explicit Lista(Nodo* _head):head(_head) {}
+    explicit Lista(Nodo<T>* _head):head(_head) {}
+    T front() {
+        return head->dato;
+    } // Retorna el elemento al comienzo
+    T back() {
+        Nodo<T>* temp = head;
+        while (temp-> next != nullptr) {
+            temp = temp->next;
+        }
+        return temp->dato;
 
+    } // Retorna el elemento al final
+    void push_front(T) {
+
+    } // Agrega un elemento al comienzo
+    void push_back(T); // Agrega un elemento al final
+    T pop_front(); // Remueve el elemento al comienzo
+    T pop_back(); // Remueve el elemento final
 
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Nodo<int> head = Nodo(5);
+    Lista lista1 = Lista(&head);
+
     return 0;
 }
