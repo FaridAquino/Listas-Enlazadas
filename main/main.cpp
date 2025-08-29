92cb19ac69d109e6f40b5dc9bf04bf5faecfdde7
R10-Mat <rayhan.matos@utec.edu.pe>
2025-08-28 22:30:42 -0500

    4 funciones incorporadas

--- main/main.cpp
+++ main/main.cpp
@@ -20,24 +20,42 @@ public:
     } // Retorna el elemento al comienzo
     T back() {
         Nodo<T>* temp = head;
-        while (temp-> next != nullptr) {
+        while (temp->next != nullptr) {
             temp = temp->next;
         }
         return temp->dato;
 
     } // Retorna el elemento al final
-    void push_front(T) {
+    void push_front(T elemento) {
+        Nodo<T>* new_node = new Nodo<T>(elemento);
+        new_node->next = head;
+        head = new_node;
 
     } // Agrega un elemento al comienzo
-    void push_back(T); // Agrega un elemento al final
-    T pop_front(); // Remueve el elemento al comienzo
-    T pop_back(); // Remueve el elemento final
+    void push_back(T elemento) {
+        Nodo<T>* new_node = new Nodo<T>(elemento);
+        Nodo<T>* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    } // Agrega un elemento al final
+    T pop_front() {
+
+    } // Remueve el elemento al comienzo
+    T pop_back() {
+
+    } // Remueve el elemento final
 
 };
 
 int main() {
-    Nodo<int> head = Nodo(5);
-    Lista lista1 = Lista(&head);
+    Nodo<int>* head = new Nodo(5);
+    Lista<int> lista1 = Lista(head);
+    lista1.push_back(10);
+    lista1.push_back(15);
+    cout << lista1.front() << endl;
+    cout << lista1.back() << endl;
 
     return 0;
 }
